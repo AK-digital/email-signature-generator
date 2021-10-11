8395a7fc281ee27d21eb453c6cb073de6aa83510
Aurélien <aurelien.kohler@gmail.com>
2021-10-11 17:10:09 +0200

    adding plugin updater

--- email-signature-generator.php
+++ email-signature-generator.php
@@ -7,7 +7,7 @@
 Plugin Name: Email signature generator (ESG)
 Plugin URI: https://studiokrack.fr
 Description: Automate html email signature generation for users
-Version: 1.0
+Version: 1.1
 Author: aurelien@studiokrack.fr
 Author URI: https://studiokrack.fr/
 License: GPLv2 or later
@@ -66,4 +66,21 @@ register_deactivation_hook(__FILE__, 'deactivate_esg_plugin');
  */
 if (class_exists('Includes\\Init')) {
     Includes\Init::register_services();
+}
+include_once('updater/updater.php');
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'email-signature-generator', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/AK-digital/email-signature-generator', // the GitHub API url of your GitHub repo
+        'raw_url' => 'https://raw.github.com/AK-digital/email-signature-generator/master', // the GitHub raw url of your GitHub repo
+        'github_url' => 'https://github.com/AK-digital/email-signature-generator', // the GitHub url of your GitHub repo
+        'zip_url' => 'https://github.com/AK-digital/email-signature-generator/zipball/master', // the zip url of the GitHub repo
+        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '1.1', // which version of WordPress does your plugin require?
+        'tested' => '1.1', // which version of WordPress is your plugin tested up to?
+        'readme' => 'email-signature-generator.php', // which file to use as the readme for the version number
+        'access_token' => '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', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
+    );
+    new WP_GitHub_Updater($config);
 }
---
+++ updater/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'radishconcepts/wordpress-github-plugin-updater',
+  ),
+  'versions' => 
+  array (
+    'radishconcepts/wordpress-github-plugin-updater' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
